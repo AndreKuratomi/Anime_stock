03acdb8e98e8d06ae974ada9152c4a2522caa73a
André Kuratomi <andrekuratomi@gmail.com>
2021-11-21 17:08:19 -0300

    feat: psycopg2-binary lib added and psycopg structure drafted

--- app/controllers/app_controllers.py
+++ app/controllers/app_controllers.py
@@ -2,16 +2,22 @@ from app.models.app_models import Animes
 
 
 def blue_get_create():
-    return Animes.get_create()
+    data = request.json()
+    new_object = Animes(**data)
+    new_object.get_create()
+    return new_object, 201
 
 
 def blue_filter():
-    return Animes.filter()
+    filtering = Animes.filter()
+    return filtering, 200
 
 
 def blue_update():
-    return Animes.update()
+    updated = Animes.update()
+    return updated, 200
 
 
 def blue_delete():
-    return Animes.delete()
+    deleted = Animes.delete()
+    return deleted, 200
--- app/models/app_models.py
+++ app/models/app_models.py
@@ -1,27 +1,81 @@
-from flask import jsonify
 from dotenv import load_dotenv
 from os import getenv
+import psycopg2
+from ipdb import set_trace
 
 load_dotenv()
 
-# coisas para tirar do .env
+configs = {
+    "host": getenv("HOST"),
+    "database": getenv("DATABASE"),
+    "user": getenv("USER"),
+    "password": getenv("password")
+}
 
 
 class Animes:
-    def __init__(self):
-        ...
+
+    fieldnames = ['id', 'anime', 'released_date', 'seasons']
+
+    def __init__(self, anime: str, released_date: str, seasons: int) -> None:
+        self.anime = anime
+        self.released_date = released_date
+        self.seasons = seasons
+
+    @staticmethod
+    def create_table():
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        cur.execute(
+            """
+                CREATE DATABASE IF NOT EXISTS animes(
+                    id 			  BIGSERIAL    PRIMARY KEY,
+                    anime 		  VARCHAR(100) NOT NULL UNIQUE,
+                    released_date DATE 		   NOT NULL,
+                    seasons 	  INTEGER 	   NOT NULL
+                );
+            """
+        )
+
+        conn.commit()
+        cur.close()
+        conn.close()
 
     def get_create(self):
-        ...
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        cur.execute()
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
 
     @staticmethod
     def filter():
-        ...
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        conn.commit()
+        cur.close()
+        conn.close()
 
     @staticmethod
     def update():
-        ...
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        conn.commit()
+        cur.close()
+        conn.close()
 
     @staticmethod
     def delete():
-        ...
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        conn.commit()
+        cur.close()
+        conn.close()
